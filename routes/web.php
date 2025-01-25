b8e0a93e9e7dcf67d5cf5a8e6e5d722b783015b8
Mohamed Kasim <mk4957454@gmail.com>
2025-01-25 11:02:19 +0200

    create request of single contact view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home', [
@@ -37,8 +38,40 @@ Route::get('/contact', function () {
         ],
     ]);
 });
+
+Route::get('/contact/{id}', function ($id)  {
+    // dd($id);
+    $data = [
+        [
+            'id' => '1',
+            'name' => 'Mohamed',
+            'phone' => '092######',
+            'email' => '@gmail.com',
+        ],
+        [
+            'id' => '2',
+            'name' => 'Ahmed',
+            'phone' => 'here is the phone number',
+            'email' => 'here is the email',
+        ],
+        [
+            'id' => '3',
+            'name' => 'Ali',
+            'phone' => 'here is the phone number',
+            'email' => 'here is the email',
+        ],
+    ];
+
+    // \Illuminate\Support\Arr::first($data, function ($item) use ($id){
+    //     return $item['id'] == $id;
+    // });
+
+    $contact = Arr::first($data, fn ($item) => $item['id'] == $id);
+    // dd($contact);
+
+    return view(view: 'cont',data: ['contactv' => $contact]);
 });
 
-Route::get('/meet-team', function() {
+Route::get('/meet-team', function () {
     return view('team');
 });
