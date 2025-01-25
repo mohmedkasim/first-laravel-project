69dc4d648d6cae37f1964df726b234cf8b543cd9
Mohamed Kasim <mk4957454@gmail.com>
2025-01-25 11:00:04 +0200

    add list foreach contact in list and view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,4 +3,12 @@
         Contact screen
     </x-slot:heading>
     <h2>Hello from contact page</h2>
+
+    @foreach($data as $item)
+    <li>
+        <a href="/contact/{{ $item['id'] }}">
+            <strong>{{ $item['name'] }}</strong>: {{ $item['email'] }}
+        </a>
+    </li>
+    @endforeach
 </x-layout>
