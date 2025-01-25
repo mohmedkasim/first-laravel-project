7880e528df6d393447d83bba2fc70a4734b4400e
Mohamed Kasim <mk4957454@gmail.com>
2025-01-25 11:10:43 +0200

    add contracts into external variables and accessable

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
+$contacts = [
+    [
+        'id' => '1',
+        'name' => 'Mohamed',
+        'phone' => '092######',
+        'email' => '@gmail.com',
+    ],
+    [
+        'id' => '2',
+        'name' => 'Ahmed',
+        'phone' => 'here is the phone number',
+        'email' => 'here is the email',
+    ],
+    [
+        'id' => '3',
+        'name' => 'Ali',
+        'phone' => 'here is the phone number',
+        'email' => 'here is the email',
+    ],
+];
+
 Route::get('/', function () {
     return view('home', [
         'greeting' => 'Hello world',
@@ -14,53 +35,15 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/contact', function () {
+Route::get('/contact', function () use ($contacts) {
     return view('contact', [
-        'data' => [
-            [
-                'id' => '1',
-                'name' => 'Mohamed',
-                'phone' => '092######',
-                'email' => '@gmail.com',
-            ],
-            [
-                'id' => '2',
-                'name' => 'Ahmed',
-                'phone' => 'here is the phone number',
-                'email' => 'here is the email',
-            ],
-            [
-                'id' => '3',
-                'name' => 'Ali',
-                'phone' => 'here is the phone number',
-                'email' => 'here is the email',
-            ],
-        ],
+        'data' =>  $contacts,
     ]);
 });
 
-Route::get('/contact/{id}', function ($id)  {
+Route::get('/contact/{id}', function ($id) use ($contacts) {
     // dd($id);
-    $data = [
-        [
-            'id' => '1',
-            'name' => 'Mohamed',
-            'phone' => '092######',
-            'email' => '@gmail.com',
-        ],
-        [
-            'id' => '2',
-            'name' => 'Ahmed',
-            'phone' => 'here is the phone number',
-            'email' => 'here is the email',
-        ],
-        [
-            'id' => '3',
-            'name' => 'Ali',
-            'phone' => 'here is the phone number',
-            'email' => 'here is the email',
-        ],
-    ];
+    $data = $contacts;
 
     // \Illuminate\Support\Arr::first($data, function ($item) use ($id){
     //     return $item['id'] == $id;
