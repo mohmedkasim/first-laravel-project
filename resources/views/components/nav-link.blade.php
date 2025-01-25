c7e622c80e5a3cf37894926b2eef53edc2f05be7
Mohamed Kasim <mk4957454@gmail.com>
2025-01-25 10:59:32 +0200

    using different if condition and passing attributes

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,15 +1,15 @@
-@props(['active' => false, 'type' => 'a'])
+{{-- @props(['active' => false, 'type' => 'a']) --}}
 {{-- props is not the same as attributes becuase props will not show in inspect instead of attributes, if we not declare the props, it will assumed as an attribute when we include attributes into tag --}}
 {{-- @php
     $active = false;
 @endphp --}}
- {{-- other solution of using if --}}
- {{-- @if ($type == 'a')
+{{-- other solution of using if --}}
+{{-- @if ($type == 'a')
  @else
  @endif --}}
-<?php if ($type == 'a') :?>
+{{-- <?php if ($type == 'a') :?> --}}
 
-    <a class="{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium "
+{{--    <a class="{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium "
         aria-current="{{  $active ? 'page' : 'false'}}" {{ $attributes }}>
         {{ $slot }}
     </a>
@@ -17,5 +17,14 @@
     <button class="{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium "
         aria-current="{{  $active ? 'page' : 'false'}}" {{ $attributes }}>
         {{ $slot }}
-    </button>
-<?php endif ?>
+    </button> --}}
+{{-- <?php endif ?> --}}
+
+
+
+@props(['active' => false])
+
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+    {{ $slot }}
+</a>
