aa605a7b6d1a8de5794f41bea76042eb65c71f59
Mohamed Kasim <mk4957454@gmail.com>
2025-01-25 11:15:50 +0200

    or add contacts data into model called Contact with all function

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,30 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
-$contacts = [
-    [
-        'id' => '1',
-        'name' => 'Mohamed',
-        'phone' => '092######',
-        'email' => '@gmail.com',
-    ],
-    [
-        'id' => '2',
-        'name' => 'Ahmed',
-        'phone' => 'here is the phone number',
-        'email' => 'here is the email',
-    ],
-    [
-        'id' => '3',
-        'name' => 'Ali',
-        'phone' => 'here is the phone number',
-        'email' => 'here is the email',
-    ],
-];
+class Contact {
+    public static function all(): array {
+        return  [
+            [
+                'id' => '1',
+                'name' => 'Mohamed',
+                'phone' => '092######',
+                'email' => '@gmail.com',
+            ],
+            [
+                'id' => '2',
+                'name' => 'Ahmed',
+                'phone' => 'here is the phone number',
+                'email' => 'here is the email',
+            ],
+            [
+                'id' => '3',
+                'name' => 'Ali',
+                'phone' => 'here is the phone number',
+                'email' => 'here is the email',
+            ],
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home', [
@@ -35,15 +39,15 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/contact', function () use ($contacts) {
+Route::get('/contact', function () {
     return view('contact', [
-        'data' =>  $contacts,
+        'data' =>  Contact::all(),
     ]);
 });
 
-Route::get('/contact/{id}', function ($id) use ($contacts) {
+Route::get('/contact/{id}', function ($id) {
     // dd($id);
-    $data = $contacts;
+    $data = Contact::all();
 
     // \Illuminate\Support\Arr::first($data, function ($item) use ($id){
     //     return $item['id'] == $id;
