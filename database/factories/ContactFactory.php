bdb1959752a62e7a8748f19639924411ff0cb219
Mohamed Kasim <mk4957454@gmail.com>
2025-02-16 00:22:02 +0200

    create contact factory

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=> fake()->name(),
+            'employer_id' => Employer::factory(),
+            'phone'=> fake()->phoneNumber(),
+            'email' =>  fake()-> email(),
+        ];
+    }
+}
